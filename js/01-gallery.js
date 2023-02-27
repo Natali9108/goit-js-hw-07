c477b1d707573f7a56155d275f01825e0df51628
Natali <114168847+Natali9108@users.noreply.github.com>
2023-02-27 22:23:17 +0200

    Створена галерея зображень

---
+++ js/01-gallery.js
@@ -0,0 +1,41 @@
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого
+//шаблону елемента галереї.
+// Реалізація делегування на div.gallery і отримання url великого
+//зображення.
+// Підключення скрипту і стилів бібліотеки модального вікна
+//basicLightbox.
+//Використовуй CDN сервіс jsdelivr і додай у проект посилання на
+//мініфіковані (.min) файли бібліотеки.
+// Відкриття модального вікна по кліку на елементі галереї. Для цього
+//ознайомся з документацією і прикладами.
+// Заміна значення атрибута src елемента <img> в модальному вікні
+//перед відкриттям. Використовуй готову розмітку модального вікна із
+//зображенням з прикладів бібліотеки basicLightbox.
+
+import { galleryItems } from "./gallery-items.js";
+
+const createGalleryCard = ({ description, original, preview }) =>
+	`<div class="gallery__item">
+    <a class="gallery__link" href='${original}'>
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source='${original}'
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+const galleryCard = galleryItems.map((el) => createGalleryCard(el)).join("");
+
+const galleryBoxRef = document.querySelector(".gallery");
+galleryBoxRef.insertAdjacentHTML("afterbegin", galleryCard);
+
+galleryBoxRef.addEventListener("click", handelGalleryItemClick);
+
+function handelGalleryItemClick(event) {
+  const isImageEl = event.target.classList.contains("gallery__image")
+	if (!isImageEl) {
+		return;
+	}
+	console.log(event.target);
+}
