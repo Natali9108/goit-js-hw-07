4861cedb17cd0e886967c3193ae5baa2ff189477
Natali <114168847+Natali9108@users.noreply.github.com>
2023-03-01 22:40:13 +0200

    реалізована бібліотека

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryBoxRef = document.querySelector(".gallery");
+
+function createGalleryCard(galleryItems) {
+	return galleryItems
+		.map(
+			({ original, preview, description }) =>
+				`<a class="gallery__item"
+	  href="${original}">
+	  <img class="gallery__image"
+	  src="${preview}" alt="${description}" />
+	</a>`
+		)
+		.join("");
+}
+
+const galleryCard = createGalleryCard(galleryItems);
+galleryBoxRef.insertAdjacentHTML("afterbegin", galleryCard);
+
+galleryBoxRef.addEventListener("click", handelGalleryItemClick);
+
+function handelGalleryItemClick(event) {
+	event.preventDefault();
+	const isImageEl = event.target.classList.contains("gallery__image");
+	if (!isImageEl) {
+		return;
+	}
+}
+
+var lightbox = new SimpleLightbox(".gallery a", {});
+lightbox.on("shown.simplelightbox", function () {
+	console.log(lightbox);
+});
